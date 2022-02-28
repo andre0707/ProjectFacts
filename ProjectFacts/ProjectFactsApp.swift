7b163bab5935787fed719998312250e3c3f4cd26
Andre <andre@aaindustries.de>
2022-02-28 16:40:29 +0100

    Initial Commit

---
+++ ProjectFacts/ProjectFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectFactsApp.swift
+//  ProjectFacts
+//
+//  Created by Andre Albach on 28.02.22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
